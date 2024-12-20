dd697dc3bc48c5a3e53bc55c265ed5bc8c65c917
chrisamirani <chrisamirani@yahoo.com>
2024-12-20 09:17:50 -0800

    test in service worker

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,19 +1,1 @@
 import "@khmyznikov/pwa-install";
-const notificationButton = document.getElementById("show-notification");
-
-notificationButton?.addEventListener("click", async () => {
-  // Request notification permission
-  const permission = await Notification.requestPermission();
-
-  if (permission === "granted") {
-    // Schedule a notification after 10 seconds
-    setTimeout(() => {
-      new Notification("Hello!", {
-        body: "This is your notification after 10 seconds.",
-        icon: "icon.png", // Optional: Path to an icon image
-      });
-    }, 10000); // 10 seconds
-  } else {
-    alert("Notification permission was denied.");
-  }
-});
